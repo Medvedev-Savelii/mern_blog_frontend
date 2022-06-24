19e7330cbbbc37dc5e71c19be853a57ca0816359
sava <savamedvedev@gmail.com>
2022-06-24 18:54:11 +0800

    four-commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
+
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -11,12 +14,14 @@ import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
